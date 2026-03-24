465e42cd94f76687e3cc29801895f0fd229538d1
carloseduduu <123980773+carloseduduu@users.noreply.github.com>
2026-03-23 21:17:04 -0300

    Refactor triangle validation logic in exemplo01.go

--- sala de aula/Exemplo Golang/exemplo01.go
+++ sala de aula/Exemplo Golang/exemplo01.go
@@ -17,9 +17,9 @@ func main() {
 		f.Print("VALORES INVÁLIDOS")
 	} else if l1 == l2 && l2 == l3 {
 		f.Print("É um triângulo equilátero")
-	} else if l1 != l2 && l2 != l3 {
+	} else if l1 != l2 && l2 != l3 && l1 != l3 {
 		f.Print("É um triângulo Escaleno")
-	} else if l1 == l2 || l1 == l3 {
+	} else if l1 == l2 || l1 == l3 || l2 == l3 {
 		f.Print("É um triângulo isóceles")
 	}
 }
