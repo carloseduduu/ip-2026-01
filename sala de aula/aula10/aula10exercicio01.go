b0146f661d3ae37ec0fa7a1348ce3492360211b4
carloseduduu <carlosfreireedu08gmail.com>
2026-04-08 14:39:19 -0300

    exercício aula 10 usando slice

--- sala de aula/aula10/aula10exercicio01.go
+++ sala de aula/aula10/aula10exercicio01.go
@@ -1,36 +1,38 @@
 package main
 
-import (
-	f "fmt"
-)
+import f "fmt"
 
 func main() {
-	type pessoa struct {
+
+	type pessoa struct { // STRUCT -> type NOME struct {}
 		nome      string
 		altura    float64
 		pesoideal float64
 	}
-	var dados pessoa
-	var limite int
 
-	f.Print("Quantas pessoas quer adicionar?")
-	f.Scan(&limite)
+	var (
+		p     pessoa
+		lista []pessoa
+	) //SLICE -> NOME []TIPO
 
-	pessoas := make([]pessoa, limite)
+	for i := 0; i >= 0; i++ {
 
-	for i := 0; i < limite; i++ {
-		if dados.nome != "FIM" {
-			f.Print("Insira seu nome: ")
-			f.Scan(&dados.nome)
-			f.Print("Insira sua altura: ")
-			f.Scan(&dados.altura)
-			dados.pesoideal = 72.7*dados.altura - 58.0
-			pessoas[i] = dados
+		f.Scan(&p.nome)
+
+		if p.nome == "FIM" {
+			break
 		}
+
+		f.Scan(&p.altura)
+		p.pesoideal = p.altura / 2.57
+		lista = append(lista, p)
+
 	}
 
-	for i := 0; i < limite; i++ {
-		f.Print(pessoas[i])
+	for i := 0; i < len(lista); i++ {
+
+		f.Printf("Nome: %v\nAltura: %v\nPeso ideal: %v\n", lista[i].nome, lista[i].altura, lista[i].pesoideal)
 	}
 
+	f.Println("FIM")
 }
