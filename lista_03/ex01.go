fe3034d184dc44ecee3b6439c8e5b6f04153be42
carloseduduu <carlosfreireedu08gmail.com>
2026-04-10 11:20:26 -0300

    teste comit via git

--- lista_03/ex01.go
+++ lista_03/ex01.go
@@ -1,28 +1,32 @@
 package main
+
 import f "fmt"
 
 func pot(base, exp int) int {
-	for i := 0; i < exp; i++{
-		resultado = base
-		resultado = resultado*base
+	resultado := base
+	for i := 1; i < exp; i++ {
+		resultado = resultado * base
 	}
 	return resultado
 }
 
-func main(){
-	var valor int
+func main() {
+	var valor, exp int
 	f.Print("Insira um valor Inteiro positivo: ")
-	f.Scan(&valor, &exp)
+	f.Scan(&valor)
+	f.Print("Insira o expoente: ")
+	f.Scan(&exp)
 	if valor <= 0 {
 		for valor <= 0 {
-		f.Print("Insira um valor Inteiro positivo: ")
-		f.Scan(&valor)
+			f.Print("Insira um valor Inteiro positivo: ")
+			f.Scan(&valor)
 
-		if valor > 0 {break}
+			if valor > 0 {
+				break
+			}
 		}
 	}
 
-	f.Printf("O valor de %v, elevado à %vª potência é igual a: %v", valor, exp, pot(valor,exp))
-	
-}
+	f.Printf("O valor de %v, elevado à %vª potência é igual a: %v\n", valor, exp, pot(valor, exp))
 
+}
