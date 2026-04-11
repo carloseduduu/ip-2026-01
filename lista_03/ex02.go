dd9b6a8899ca96fad6c66454a8103da56dd0353b
carloseduduu <carlosfreireedu08@gmail.com>
2026-04-11 11:59:28 -0300

    teste commit

--- lista_03/ex02.go
+++ lista_03/ex02.go
@@ -4,12 +4,16 @@ import f "fmt"
 
 func main() {
 	var soma int = 0
+	var i int = 0
 	valor := 50
 	for valor = 50; valor <= 70; valor++ {
 		if valor%2 == 0 {
 			soma = soma + valor
-			f.Println(soma)
+			i++
+			f.Printf("A soma na posição %v é %v\n", i, soma)
 		}
 
 	}
+	media := soma / i
+	f.Printf("A média dos valores pares entre 50 e 70 é %v\n", media)
 }
