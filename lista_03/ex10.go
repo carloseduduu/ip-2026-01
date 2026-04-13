8d1adf320ef4100ebfd17cc410c813ef165480a2
carloseduduu <carlosfreireedu08gmail.com>
2026-04-13 19:25:29 -0300

    ex10

--- lista_03/ex10.go
+++ lista_03/ex10.go
@@ -4,15 +4,19 @@ import f "fmt"
 
 func main() {
 	var (
-		n, ant, termo int
+		n      int
+		n1, n2 = 0, 1
 	)
 
 	f.Print("Insira o valor de N termos: ")
 	f.Scan(&n)
 
-	for i := 1; i <= n; i++ {
-		ant = termo
-		termo = ant + i
-		f.Printf("%d-", termo)
+	f.Printf("%d - %d - ", n1, n2)
+
+	for i := 2; i <= n; i++ {
+		atual := n1 + n2
+		n1 = n2
+		n2 = atual
+		f.Printf("%d - ", atual)
 	}
 }
